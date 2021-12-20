03948a7aa16cbbab71c57eb3d7cb80fb66604d1b
sulthanfadli <73649250+sulthanfadli@users.noreply.github.com>
2021-12-20 10:43:29 +0700

    new

--- app.py
+++ app.py
@@ -53,6 +53,17 @@ def predict(filename, model):
 def home():
     return render_template("index.html")
 
+@app.route('/tools')
+def tools():
+    return render_template("tools.html")
+
+@app.route('/plans')
+def plans():
+    return render_template("plans.html")
+
+@app.route('/events')
+def events():
+    return render_template("events.html")
 
 @app.route('/success', methods=['GET', 'POST'])
 def success():
@@ -82,14 +93,25 @@ def success():
                     "prob3": prob_result[2],
                 }
 
+                if predictions == "Breccia (Breksi)":
+                    desctription = "Breccia"
+                elif predictions == "Claystone (Batulempung)":
+                    desctription =  "Claystone"
+                elif predictions == "Conglomerate (Konglomerat)":
+                    desctription = "Conglomerate"
+                elif predictions == "Sandstone (batupasir)":
+                    desctription = "Sandstone"
+                else:
+                    desctription = "Siltstone"
+
             except Exception as e:
                 print(str(e))
                 error = 'This image from this site is not accesible or inappropriate input'
 
             if (len(error) == 0):
-                return render_template('success.html', img=img, predictions=predictions)
+                return render_template('success.html', img=img, predictions=predictions, desctription=desctription)
             else:
-                return render_template('index.html', error=error)
+                return render_template('tools.html', error=error)
 
 
         elif (request.files):
@@ -110,16 +132,65 @@ def success():
                     "prob3": prob_result[2],
                 }
 
+                if predictions['class1']  == "Breccia (Breksi)":
+                    warna =" hijau kekuningan atau coklat keputih-putihan."
+                    ukuran = ">2milimeter "
+                    kemas = "Terbuka"
+                    sortasi = "Buruk "
+                    bentuk = "Angular (menyudut) "
+                    porosity = "10-30% "
+                    permeability = "Moderate"
+                elif predictions['class1']  == "Claystone (Batulempung)":
+                    warna = "warna : warna segar kuning kecoklatan, warna lapuk coklat kehitaman."
+                    ukuran = "ukuran butir : >2milimeter "
+                    kemas = "kemas : Terbuka"
+                    sortasi = "sortasi : Buruk"
+                    bentuk = "bentuk butir : Rounded (membundar)"
+                    porosity = "porosity : 10-30%"
+                    permeability = "permeability : Moderate "
+                elif predictions['class1']  == "Conglomerate (Konglomerat)":
+                    warna = "warna : coklat muda, coklat, kuning, merah, abu-abu dan putih."
+                    ukuran = "ukuran butir : 1/16-2 milimeter"
+                    kemas = "kemas : Tertutup"
+                    sortasi = "sortasi : Sedang"
+                    bentuk = "bentuk butir : Rounded (membundar)"
+                    porosity = "porosity : 14-49%"
+                    permeability = "permeability : Moderate "
+                elif predictions['class1']   == "Sandstone (batupasir)":
+                    warna = "warna : abu-abu, coklat, atau coklat kemerahan. Kadang juga berwarna putih, kuning, hijau, merah, ungu, oranye, hitam, dan lainnya."
+                    ukuran = "ukuran butir : 1/256-1/16 milimeter"
+                    kemas = "kemas : Tertutup"
+                    sortasi = "sortasi : Baik"
+                    bentuk = "Rounded (membundar)"
+                    porosity = "porosity : 21-41%"
+                    permeability = "permeability : Moderate "
+                elif predictions['class1']   == "Siltstone (Batulanau)":
+                    warna = "warna segar abu-abu, warna lapuk abu-abu kecoklatan."
+                    ukuran = "< 1/256 milimeter"
+                    kemas = "Tertutup"
+                    sortasi = "Baik"
+                    bentuk = "Rounded (membundar)"
+                    porosity = "41-45%"
+                    permeability = "Moderate "
+                else:
+                    warna = "-"
+                    ukuran = "-"
+                    kemas = "-"
+                    sortasi = "-"
+                    bentuk = "-"
+                    porosity = "-"
+                    permeability = "-"
+
             else:
                 error = "Please upload images of jpg , jpeg and png extension only"
 
             if (len(error) == 0):
-                return render_template('success.html', img=img, predictions=predictions)
+                return render_template('success.html', img=img, predictions=predictions, warna=warna, ukuran=ukuran, kemas=kemas, sortasi=sortasi, bentuk=bentuk, porosity=porosity, permeability=permeability)
             else:
-                return render_template('index.html', error=error)
+                return render_template('tools.html', error=error)
 
     else:
-        return render_template('index.html')
+        return render_template('tools.html')
 
 
 if __name__ == "__main__":
